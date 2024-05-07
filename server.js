50fadfc1e77d04e1b1a3fb9c2aeb1cc9b475f573
liviagalletti <liligalletti@gmail.com>
2024-05-07 09:39:29 -0300

    server

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  View Engine and Templates
@@ -22,9 +24,27 @@ const static = require("./routes/static")
  *************************/
 app.use(static)
 //Index route
-app.get("/", function(req, res){
-  res.render("index", {title:"Home"})
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
