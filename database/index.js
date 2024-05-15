5e848208b6de61a7fb0b35e11a3740053bbd45ef
liviagalletti <liligalletti@gmail.com>
2024-05-15 18:08:45 -0300

    pool

--- database/index.js
+++ database/index.js
@@ -7,14 +7,13 @@ require("dotenv").config()
  * If - else will make determination which to use
  * *************** */
 let pool
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV == "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
-  });
-
+})
 
 // Added for troubleshooting queries
 // during development
@@ -34,6 +33,5 @@ module.exports = {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
   })
- }
-
-module.exports = pool
+  module.exports = pool
+}
